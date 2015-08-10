bfcdfdb65b41a4224d9fcb1b12e390da0679b0f4
Quang vu <nickiewooster@gmail.com>
2015-08-10 04:21:44 +0200

    added signin ui

--- webapp/components/authentication/signin.jsx
+++ webapp/components/authentication/signin.jsx
@@ -4,11 +4,21 @@ var React = require('react');
 var Signin = React.createClass({
   render: function() {
     return (
-      <div className="signin">
-        <form action="submit">
-			<input type="text"/>
-			<input type="password"/>
-			<button>Submit</button>
+      <div className="signin z-depth-1">
+        <form>
+    			<div className="input-field">
+            <i className="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" className="validate" required/>
+            <label for="icon_prefix">Username</label>
+          </div>
+          <div className="input-field">
+            <i className="material-icons prefix">lock_outline</i>
+            <input id="icon_prefix" type="password" className="validate" required/>
+            <label for="icon_prefix">Password</label>
+          </div>
+
+          <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+          </button>
         </form>
       </div>
     );
